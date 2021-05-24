1cd352641bb74975ee45a41220c33c5e9f994305
maitlandmarshall <maitlandmarshallapps@gmail.com>
2021-05-24 10:20:48 +1000

    Implement NullableDatePicker

---
+++ MAD.XamarinForms.NullableControls/NullableDatePicker.shared.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MAD.XamarinForms.NullableControls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NullableDatePicker : ContentView
+    {
+        public static BindableProperty DateProperty = BindableProperty.Create(nameof(Date), typeof(DateTime?), typeof(NullableDatePicker), defaultBindingMode: BindingMode.TwoWay);
+        public static BindableProperty FormatProperty = BindableProperty.Create(nameof(Format), typeof(string), typeof(NullableDatePicker), "D");
+        public static BindableProperty BorderColorProperty = BindableProperty.Create(nameof(BorderColor), typeof(Color), typeof(NullableDatePicker), Color.FromHex("#666666"));
+
+        public NullableDatePicker()
+        {
+            InitializeComponent();
+        }
+
+        public DateTime? Date
+        {
+            get => this.GetValue(DateProperty) as DateTime?;
+            set => this.SetValue(DateProperty, value);
+        }
+
+        public string Format
+        {
+            get => this.GetValue(FormatProperty) as string;
+            set => this.SetValue(FormatProperty, value);
+        }
+
+        public Color BorderColor
+        {
+            get => (Color)this.GetValue(BorderColorProperty);
+            set => this.SetValue(BorderColorProperty, value);
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            this.HiddenDatePicker.Focus();
+        }
+
+        private void HiddenDatePicker_Unfocused(object sender, FocusEventArgs e)
+        {
+            this.Date = this.HiddenDatePicker.Date;
+        }
+    }
+}
---
+++ MAD.XamarinForms.NullableControls/NullableToBoolConverter.shared.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MAD.XamarinForms.NullableControls
+{
+    internal class NullableToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is null == false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
